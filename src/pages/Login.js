b2b01fdd7f59c0920b84adb35d35448fdaf49cb7
Deniz Ozturk <ozturk.deniz@protonmail.com>
2024-06-29 21:32:22 +0300

    Login Authentication Added
    Users aren't allowed to create users. Manually added by admins only

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,18 +3,34 @@ import React from 'react'
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+/* import {
+  HttpsError,
+  beforeUserCreated
+} from "firebase-functions/v2/identity"; */
+
 
 function Login({setIsAuth}) {
   
   let navigate = useNavigate();
 
+  /* const beforecreated = beforeUserCreated((event) => {
+    const user = event.data;
+    if (user.email && !user.emailVerified) {
+      throw new HttpsError(
+        'invalid-argument', 'Unverified email');
+    }
+      return;
+  }); */
+  
   const signInWithGoogle = () => {
+    /* beforecreated(); */
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true)
       setIsAuth(true);
       navigate("/");
     }).catch((err) => {
-      alert(err);
+      debugger;
+      alert(t("notAuth"));
     });
   };
   return (
